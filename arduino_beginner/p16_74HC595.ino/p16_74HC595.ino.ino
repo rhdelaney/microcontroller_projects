c96d56abe5f9e2cd7a8220f51e1a9f57a5d5a213
Raychel <rhdelaney@crimson.ua.edu>
2020-03-07 18:22:32 -0800

    And we are done with 20, 12 more practice beginner problems to go :)

---
+++ arduino_beginner/p16_74HC595.ino/p16_74HC595.ino.ino
@@ -0,0 +1,25 @@
+int data = 2;// set pin 14 of 74HC595as data input pin SI 
+int clock = 5;// set pin 11 of 74hc595 as clock pin SCK
+int latch = 4;// set pin 12 of 74hc595 as output latch RCK 
+int ledState = 0;
+
+const int ON = HIGH;
+const int OFF = LOW;
+
+void setup() {
+  pinMode(data, OUTPUT);
+  pinMode(clock, OUTPUT);
+  pinMode(latch, OUTPUT);
+}
+
+void loop() {
+  for (int i = 0; i < 256; i++) {
+    updateLEDs(i);
+    delay(500);
+  }
+}
+void updateLEDs(int value) {
+  digitalWrite(latch, LOW);//
+  shiftOut(data, clock, MSBFIRST, ~value);// serial data “output”, high level first
+  digitalWrite(latch, HIGH);// latch
+}
