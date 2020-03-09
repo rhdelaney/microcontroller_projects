d52f14287d367a0ee9665b50593b83c3d30d0d86
Raychel <rhdelaney@crimson.ua.edu>
2020-03-08 18:22:29 -0700

    Knockin out more projects

---
+++ arduino_beginner/p24_temperature_and_humidity.ino/p24_temperature_and_humidity.ino.ino
@@ -0,0 +1,36 @@
+#include "dht.h"
+#define dht_apin A0 // Analog Pin sensor is connected to
+ 
+dht DHT;
+double tempVal = 0.0;
+ 
+void setup(){
+ 
+  Serial.begin(9600);
+  delay(500);//Delay to let system boot
+  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
+  delay(1000);//Wait before accessing Sensor
+ 
+}//end "setup()"
+ 
+void loop(){
+  //Start of Program 
+ 
+    DHT.read11(dht_apin);
+    
+    Serial.print("Current humidity = ");
+    Serial.print(DHT.humidity);
+    Serial.print("%  ");
+    Serial.print("temperature = ");
+    tempVal = (DHT.temperature * 9 / 5) + 32;
+    Serial.print(tempVal);
+    Serial.print("F  ");
+    Serial.print("temperature = ");
+    Serial.print(DHT.temperature); 
+    Serial.println("C  ");
+    
+    delay(5000);//Wait 5 seconds before accessing sensor again.
+ 
+  //Fastest should be once every two seconds.
+ 
+}// end loop() 
